acb693f1bfb9ff97bcab9bb809c80b8cd7939f88
janio02011998 <carvalho02011998@gmail.com>
2020-08-24 21:26:13 -0300

    Amazing advance in the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,28 +1,44 @@
 import React from 'react';
+import { GrMail, GrGithub, GrFacebook, GrLinkedin, GrTwitter, GrInstagram } from 'react-icons/gr';
+import { FaBeer, FaHeart, FaBootstrap } from 'react-icons/fa';
 
-import { Container, SidebarLink } from './styles';
+// import { Container, SidebarLink } from './styles';
 
 function Sidebar() {
     return (
         <div>
-            <nav class="navbar" href="#navbar" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="true">
-                <aside>
-                    <div className="text-center">
-                        <img className="rounded-circle" alt="250x250" src={require('../../images/about.jpg')} />
-                        <h1><a className="btn btn-lg font-weight-bold" href="index.html" >Jânio Carvalho</a></h1>
-                        <span><i class="fas fa-envelope"></i>janiocarvalhojr@gmail.com</span>
+            <nav href="#navbar" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" aria-label="Toggle navigation"><i /></nav>
+            <aside>
+                <div className="text-center">
+                    <img alt="100X100" className="img-fluid img-thumbnail rounded-circle" src={require('../../images/about.jpg')} />
+                    <h1><a className="btn btn-lg font-weight-bold" href="index.html" >Jânio Carvalho</a></h1>
+                    <span><GrMail /> janiocarvalhojr@gmail.com</span>
+                </div>
+                <nav className="nav justify-content-center" role="navigation">
+                    <div id="navbar" className="text-center">
+                        <ul className="nav flex-column">
+                            <li className="nav-item"><a href="#INTRODUCTION" className="nav-link">INTRODUCTION</a></li>
+                            <li className="nav-item"><a href="#ABOUT" className="nav-link">ABOUT</a></li>
+                            <li className="nav-item"><a href="#TIMELINE" className="nav-link">TIMELINE</a></li>
+                        </ul>
                     </div>
-                    <nav role="navigation">
-                        <div className="text-center">
-                            <ul className="nav flex-column">
-                                <li className="nav-item"><a className="nav-link">INTRODUCTION</a></li>
-                                <li className="nav-item"><a className="nav-link">ABOUT</a></li>
-                                <li className="nav-item"><a className="nav-link">TIMELINE</a></li>
-                            </ul>
-                        </div>
-                    </nav>
-                </aside>
-            </nav>
+                </nav>
+                <nav className="nav justify-content-center">
+                    <ul className="nav flex-column">
+                        <li><a className="btn" href="https://www.facebook.com/Janio20/" target="_blank" rel="noopener noreferrer"><GrFacebook /></a></li>
+                        <li><a className="btn" href="https://twitter.com/Janio29255550" target="_blank" rel="noopener noreferrer"><GrTwitter /></a></li>
+                        <li><a className="btn" href="https://www.instagram.com/janiojunior_/" target="_blank" rel="noopener noreferrer"><GrInstagram /></a></li>
+                        <li><a className="btn" href="https://www.linkedin.com/in/janio-carvalho-95966016b/" target="_blank" rel="noopener noreferrer"><GrLinkedin /></a></li>
+                        <li><a className="btn" href="https://github.com/janio02011998" target="_blank" rel="noopener noreferrer"><GrGithub /></a></li>
+                    </ul>
+                </nav>
+                <div className="text-center">
+                    <p><small>
+                        Feito com <FaHeart /> and <FaBeer /> <br />
+                            Obrigado <FaBootstrap /> pela inspiração
+                        </small></p>
+                </div>
+            </aside>
         </div>
         // <Container>
         //     <div class="container my-4">
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -3,20 +3,20 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Container = styled.aside`
-    display: flex;
+    ${'' /* display: flex;
     flex-shrink: 1;
     align-items: center;
     width: 300px;
     height: 100%;
     background-image: linear-gradient(to bottom, #D5D8DC, #17202A);
-    ${'' /* background-image: #EBEDEF; */}
+    background-image: #EBEDEF;
     flex-direction: column;
 
     transition: all 0.2s ease 0s;
 
     ${'' /* &:hover{
         width: 240px;
-    } */}
+    } */} */}
 `;
 
 export const SidebarLink = styled(NavLink)`
