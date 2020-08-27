c7c4855160adc0042bcf964f366eb4ad9e91c263
janio02011998 <carvalho02011998@gmail.com>
2020-08-26 22:04:22 -0300

    Details for  sidebar to be done.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,41 +13,44 @@ export default class Sidebar extends Component {
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div>
+                            <div className="mb-2 text-center">
+                                <img className="img-fluid img-thumbnail rounded-circle" alt="Responsive" src={require('../../images/about.jpg')} />
+                                <h1><a className="btn btn-lg font-weight-bold" href="/">Jânio Carvalho</a></h1>
+                                <span className="badge badge-light"><GrMail /> janiocarvalhojr@gmail.com</span>
+                                <span className="badge badge-light"><GrPhone /> +55 73 99179-7661</span>
+                            </div>
+                            <nav className="nav justify-content-center" role="navigation">
+                                <div className="text-center">
+                                    <ul className="nav flex-column">
+                                        <li className="nav-item"><a className="btn btn-lg-2" href="/">INTRODUCTION</a></li>
+                                        <li className="nav-item"><a className="btn btn-lg-2" href="/about">ABOUT</a></li>
+                                        <li className="nav-item"><a className="btn btn-lg-2" href="/timeline">TIMELINE</a></li>
+                                        <li className="nav-item"><a className="btn btn-lg-2" href="/contact">CONTACT</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                            <nav className="nav justify-content-center">
+                                <ul className="nav flex-column">
+                                    <li><a className="btn" href="https://www.facebook.com/Janio20/" target="_blank" rel="noopener noreferrer"><GrFacebook /></a></li>
+                                    <li><a className="btn" href="https://twitter.com/Janio29255550" target="_blank" rel="noopener noreferrer"><GrTwitter /></a></li>
+                                    <li><a className="btn" href="https://www.instagram.com/janiojunior_/" target="_blank" rel="noopener noreferrer"><GrInstagram /></a></li>
+                                    <li><a className="btn" href="https://www.linkedin.com/in/janio-carvalho-95966016b/" target="_blank" rel="noopener noreferrer"><GrLinkedin /></a></li>
+                                    <li><a className="btn" href="https://github.com/janio02011998" target="_blank" rel="noopener noreferrer"><GrGithub /></a></li>
+                                </ul>
+                            </nav>
+                            <div className="text-center">
+                                <p><small>
+                                    Feito com <FaHeart /> and <FaBeer /> <br />
+                                    Obrigado <FaBootstrap /> pela inspiração
+                                </small></p>
+                            </div>
+                        </div>
+                    </div>
                 </nav>
 
-                <div className="" id="navbarSupportedContent">
-                    <div className="mb-2 text-center">
-                        <img className="img-fluid img-thumbnail rounded-circle" alt="Responsive" src={require('../../images/about.jpg')} />
-                        <h1><a className="btn btn-lg font-weight-bold" href="/">Jânio Carvalho</a></h1>
-                        <span className="badge badge-light"><GrMail /> janiocarvalhojr@gmail.com</span>
-                        <span className="badge badge-light"><GrPhone /> +55 73 99179-7661</span>
-                    </div>  
-                    <nav className="nav justify-content-center" role="navigation">
-                        <div className="text-center">
-                            <ul className="nav flex-column">
-                                <li className="nav-item"><a className="btn btn-lg-2" href="/">INTRODUCTION</a></li>
-                                <li className="nav-item"><a className="btn btn-lg-2" href="/about">ABOUT</a></li>
-                                <li className="nav-item"><a className="btn btn-lg-2" href="/timeline">TIMELINE</a></li>
-                                <li className="nav-item"><a className="btn btn-lg-2" href="/contact">CONTACT</a></li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <nav className="nav justify-content-center">
-                        <ul className="nav flex-column">
-                            <li><a className="btn" href="https://www.facebook.com/Janio20/" target="_blank" rel="noopener noreferrer"><GrFacebook /></a></li>
-                            <li><a className="btn" href="https://twitter.com/Janio29255550" target="_blank" rel="noopener noreferrer"><GrTwitter /></a></li>
-                            <li><a className="btn" href="https://www.instagram.com/janiojunior_/" target="_blank" rel="noopener noreferrer"><GrInstagram /></a></li>
-                            <li><a className="btn" href="https://www.linkedin.com/in/janio-carvalho-95966016b/" target="_blank" rel="noopener noreferrer"><GrLinkedin /></a></li>
-                            <li><a className="btn" href="https://github.com/janio02011998" target="_blank" rel="noopener noreferrer"><GrGithub /></a></li>
-                        </ul>
-                    </nav>
-                    <div className="text-center">
-                        <p><small>
-                            Feito com <FaHeart /> and <FaBeer /> <br />
-                            Obrigado <FaBootstrap /> pela inspiração
-                        </small></p>
-                    </div>
-                </div>
             </div>
             // <Container>
             //     <div class="container my-4">
