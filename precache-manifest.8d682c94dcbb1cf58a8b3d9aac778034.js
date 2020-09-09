55877fe34cd4c35caf3f1829db57fccf257fe9dc
janio02011998 <carvalho02011998@gmail.com>
2020-09-08 22:12:46 -0300

    Updates

---
+++ precache-manifest.8d682c94dcbb1cf58a8b3d9aac778034.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bd9d7a8eb095cb85e77fc3637de757",
+    "url": "/janio02011998/portfolio/index.html"
+  },
+  {
+    "revision": "05ee22c66dcadde1618b",
+    "url": "/janio02011998/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "590ad056ea6fc8d7420c",
+    "url": "/janio02011998/portfolio/static/css/main.c2abc4c1.chunk.css"
+  },
+  {
+    "revision": "05ee22c66dcadde1618b",
+    "url": "/janio02011998/portfolio/static/js/2.c6b34886.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/janio02011998/portfolio/static/js/2.c6b34886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590ad056ea6fc8d7420c",
+    "url": "/janio02011998/portfolio/static/js/main.260a23ca.chunk.js"
+  },
+  {
+    "revision": "8f18c1a38b8c6a845ca9",
+    "url": "/janio02011998/portfolio/static/js/runtime-main.5b60b1df.js"
+  },
+  {
+    "revision": "a7b3009de7f308941d81f5d27eb42863",
+    "url": "/janio02011998/portfolio/static/media/about.a7b3009d.jpg"
+  }
+]);
